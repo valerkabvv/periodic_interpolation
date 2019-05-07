a0249a1605024fd06b72a835d14222dea779ce81
Валера Белоцкий <valerka_bvv@MacBook-Pro-Valera.local>
2019-05-08 02:33:58 +0400

    knot vector for odd degres is working!!!!!!!

--- venv/iteropolation.py
+++ venv/iteropolation.py
@@ -4,23 +4,13 @@ import math
 from scipy import linalg
 
 
-def make_knots(points, rightBorder ,k):
-    if len(points)<k:
-        raise ValueError('at least %d point',k+1)
-    if(len(points)==k+1):
-        t = np.asarray([points[0],points[-1]+(points[0]+points[-1])/(k+1)])
-    else:
-        t=(np.asarray(points[:len(points)-k-1],float)+np.asarray(points[1:len(points)-k],float))/2
-        t= np.concatenate((np.asarray([points[0]]),t,np.asarray([points[-1]+(rightBorder-points[-1])/3])))
-    t=np.concatenate((t,np.linspace(t[-1],rightBorder,k+1)[1:]))
-
-    #making knots outside of base interval
-
-    per = t[-1]-t[0]
-    t_1=np.asarray([x-per for x in t[-k-1:-1]])
-    t_2=np.asarray([x+per for x in t[1:k+1]])
-
-    return np.concatenate((t_1,t,t_2))
+def make_knots(points, k):
+    per = points[-1] - points[0]
+    points = np.asarray(points, dtype=float)
+    t_midle = np.concatenate((points[:1], points[k//2+1:-k//2-1], np.linspace(points[-2], points[-1], k+1)))
+    t_1 = np.asarray([x - per for x in t_midle[-k-1:-1]])
+    t_2 = np.asarray([x + per for x in t_midle[1:k+1]])
+    return np.concatenate((t_1, t_midle, t_2))
 
 
 def make_knots3(points):
@@ -37,7 +27,7 @@ def make_knots3(points):
 
 def make_periodic_spline(x,y,k):
     #t=make_knots(x[:-1],x[-1],k)
-    t = make_knots3(x)
+    t = make_knots(x, k)
     interp_matrix = np.zeros(shape=(len(x)-1,len(x)-1))
     bsplines=[]
     for i in [t[i:i+k+2] for i in range(len(x)-1)]:

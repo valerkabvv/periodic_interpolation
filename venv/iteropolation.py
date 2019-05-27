cdb7bef2d085222fd89aba4c8dbeb7f83678e3e4
Валера Белоцкий <valerka_bvv@MacBook-Pro-Valera.local>
2019-05-27 17:01:37 +0300

    yay

--- venv/iteropolation.py
+++ venv/iteropolation.py
@@ -1,4 +1,5 @@
 import scipy.interpolate._bsplines as interpolate
+import scipy.interpolate._bspl as bspl
 import numpy as np
 import math
 from scipy import linalg
@@ -9,31 +10,19 @@ def make_knots(points, k):
     points = np.asarray(points, dtype=float)
     t_midle = np.concatenate(
         (points[:1], points[k // 2 + 1:-k // 2 - 1], np.linspace(points[-2], points[-1], k + 1)))
-    # else:
-    #     t_midle = np.concatenate(
-    #         (points[:1], points[k // 2 + 1:-k // 2 -1],
-    #          np.linspace(points[-2], points[-1], k + 1))
-    #     )
     t_1 = np.asarray([x - per for x in t_midle[-k - 1:-1]])
     t_2 = np.asarray([x + per for x in t_midle[1:k + 1]])
     return np.concatenate((t_1, t_midle, t_2))
 
 
 
-def make_knots3(points):
-    per = points[-1]-points[0]
-    points=np.asarray(points, dtype=float)
-    t_midle=np.concatenate((points[:1],points[2:-3],np.linspace(points[-2],points[-1],4)))
-    t_1 = np.asarray([x-per for x in t_midle[-4:-1]])
-    t_2 = np.asarray([x + per for x in t_midle[1:4]])
-    # t_midle = points[2:-3]
-    # np.append(t_midle,points[-2])
-    return np.concatenate((t_1,t_midle,t_2))
-
-
-
 def make_periodic_spline(x,y,k):
     t = make_knots(x, k)
+    x_1=interpolate._as_float_array(x[:-1])
+    t_1=interpolate._as_float_array(t[:-k])
+    banded_storage = np.zeros((3*k+1,len(y)-1),dtype=np.float_,order='F')
+    bspl._colloc(x_1,t_1,k,banded_storage,offset=0)
+
     interp_matrix = np.zeros(shape=(len(x)-1,len(x)-1))
     bsplines=[]
     for i in [t[i:i+k+2] for i in range(len(x)-1)]:
@@ -43,7 +32,6 @@ def make_periodic_spline(x,y,k):
         for j in range(len(x)-1):
             if not math.isnan(bsplines[j](x[i])):
                 interp_matrix[i][j]=bsplines[j](x[i])
-
 
     y = np.asarray(y[:-1])
     c = np.linalg.solve(interp_matrix,y)
--- venv/test_interpolate_pytest.py
+++ venv/test_interpolate_pytest.py
@@ -2,6 +2,7 @@ from iteropolation import make_knots, make_periodic_spline
 from numpy.testing import assert_allclose
 import numpy as np
 from scipy.interpolate._cubic import CubicSpline
+import matplotlib.pyplot as plt
 
 
 
@@ -22,13 +23,15 @@ def test_make_periodic_spline():
     assert_allclose([spl(x[0],i) for i in range(k)], [spl(x[-1],i) for i in range(k)])
 
 def test_make_periodic_spline_sin():
-    x=np.linspace(0,2*np.pi,60)
+    x=np.linspace(0,2*np.pi,100)
     y=np.sin(x)
     k=3
     spl = make_periodic_spline(x, y, k)
     spl_value=np.asarray([spl(i) for i in x[:-1]])
     assert_allclose(y[:-1], spl_value,atol=1e-14)
     assert_allclose([spl(x[0], i) for i in range(k)], [spl(x[-1], i) for i in range(k)],atol=1e-14)
+
+
 
 
 def test_agais_CubicSpline():
@@ -41,3 +44,17 @@ def test_agais_CubicSpline():
     cubic_value = np.asarray([cub_spl(i) for i in x[:-1]])
     assert_allclose(cubic_value, spl_value, atol=1e-14)
     assert_allclose([spl(x[0], i) for i in range(k)], [spl(x[-1], i) for i in range(k)], atol=1e-14)
+
+
+def test_plot_agaist_CubicSpline():
+    x = [1, 3, 5, 6, 7, 10, 11.5,12,16,19,20]
+    k = 3
+    y = [2, 1, 4, 3, 6, 7, 4,2,1,5,2]
+    spl = make_periodic_spline(x, y, k)
+    cub_spl = CubicSpline(x, y, bc_type='periodic')
+    _, ax = plt.subplots()
+    x2 = np.linspace(1, 20, 300)
+    ax.plot(x2, cub_spl(x2), color='red')
+    ax.plot(x2, spl(x2))
+    _.show()
+    _.savefig('cubicSpline1.png')
